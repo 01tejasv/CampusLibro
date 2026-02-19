b8875ba6d28aa74fe13fa2ee195e0ce103529886
Tejasv Chauhan <tejasv.chauhan001@gmail.com>
2026-02-19 19:01:25 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4820649096-cb960",
+  "appId": "1:223771954699:web:ddd13c10d43b1de725010e",
+  "apiKey": "AIzaSyDg8XkRJ3-ySpjR594opdf1ygutHNLbYBk",
+  "authDomain": "studio-4820649096-cb960.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "223771954699"
+};
